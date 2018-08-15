609e6e5f8edcebc85f508128e45749b6ba4d7a9a
Guilherme Brendel <guilherme.brendel@owlbytes.com>
2018-08-15 00:55:25 -0300

    socket connection working

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ var express = require('express');
 var app = express();
 var serve = require('http').Server(app);
 
+
+
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/client/index.html');
 });
 
-app.use('client/' , express.static(__dirname + '/client'));
+app.use('/client' , express.static(__dirname + '/client'));
 
-console.log("Listen on Port: 2000");
 serve.listen(2000);
+console.log("Listen on Port: 2000");
+
+let io = require('socket.io') (serve,{});
+io.sockets.on('connection', function(socket) {
+    console.log('socket connection'); //whenerver a connection happens this will be called
+});
